a77900cd8bcc711601d0481fabb3c51673297cb8
hwangbitchan <hwangbitchan@naver.com>
2022-05-28 18:05:06 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/oumsback/OumsBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.oumsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OumsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OumsBackApplication.class, args);
+    }
+
+}
