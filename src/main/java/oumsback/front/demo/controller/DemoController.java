8689a1879ebdddc9a10a490bcc927f1e3b8c9959
hwangbitchan <hwangbitchan@lfpartners.co.kr>
2022-07-11 14:52:53 +0900

    rest api (post) demo 작성

--- src/main/java/oumsback/front/demo/controller/DemoController.java
+++ src/main/java/oumsback/front/demo/controller/DemoController.java
@@ -3,9 +3,9 @@ package oumsback.front.demo.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import oumsback.front.demo.dto.request.DemoRequestDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +17,11 @@ public class DemoController {
 	@GetMapping(value = "/demo")
 	public String getApi() {
 		return "hello";
+	}
+
+	@ApiOperation(value = "API 연결 테스트 (POST)", notes = "API 연결 테스트 (POST)")
+	@PostMapping(value = "/demo/post", produces = "application/json; charset=utf8")
+	public ResponseEntity<DemoRequestDTO> postDemoApi(@RequestBody DemoRequestDTO demoRequestDTO) {
+		return ResponseEntity.ok().body(demoRequestDTO);
 	}
 }
--- src/main/java/oumsback/front/demo/dto/request/DemoRequestDTO.java
+++ src/main/java/oumsback/front/demo/dto/request/DemoRequestDTO.java
@@ -1,4 +1,15 @@
 package oumsback.front.demo.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@ApiModel
+@Data
 public class DemoRequestDTO {
+	@ApiModelProperty(notes = "제목", example = "제목을 입력하세요.")
+	public String title;
+
+	@ApiModelProperty(notes = "내용", example = "내용을 입력하세요.")
+	public String content;
 }
